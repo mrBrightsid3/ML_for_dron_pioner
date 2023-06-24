24bcacc9cec4c2845db40c0f3e094a1eff813f17
egg227 <sitnikov-vanyusha@list.ru>
2023-06-24 11:03:56 +0300

    add requiments.txt + func detection_of_bottle

--- main.py
+++ main.py
@@ -14,6 +14,27 @@ sums_of_rewards = [0] * d
 dict_of_commands = {0: "w", 1: "s", 2: "a", 3: "d", 4: "q", 5: "e", 6: "i", 7: "k"}
 
 
+def detection_of_bottle():
+    frame = camera.get_frame()
+    if frame is not None:
+        camera_frame = cv2.imdecode(
+            np.frombuffer(frame, dtype=np.uint8), cv2.IMREAD_COLOR
+        )
+        if FLAG:
+            print("working...")
+            blob = cv2.dnn.blobFromImage(
+                camera_frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False
+            )
+            net.setInput(blob)
+            outs = net.forward(output_layers)
+            for out in outs:
+                for detection in out:
+                    scores = detection[5:]
+                    class_id = np.argmax(scores)
+                    confidence = scores[class_id]
+                    return confidence, class_id, camera_frame
+
+
 def ucb_where_to_fly(square, n=0):
     ad = 0
     max_upper_bound = 0
@@ -44,6 +65,8 @@ if __name__ == "__main__":
     2 -- disarm
     3 -- takeoff
     4 -- land
+    b -- check battery
+    space -- on/off autopilot
     ↶q  w↑  e↷    i-↑
     ←a      d→     k-↓
         s↓"""
@@ -62,50 +85,36 @@ if __name__ == "__main__":
             ch_3 = 1500
             ch_4 = 1500
             ch_5 = 2000
-            frame = camera.get_frame()
-            if frame is not None:
-                camera_frame = cv2.imdecode(
-                    np.frombuffer(frame, dtype=np.uint8), cv2.IMREAD_COLOR
-                )
-                if FLAG:
-                    print("working...")
-                    blob = cv2.dnn.blobFromImage(
-                        camera_frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False
-                    )
-                    net.setInput(blob)
-                    outs = net.forward(output_layers)
-                    for out in outs:
-                        for detection in out:
-                            scores = detection[5:]
-                            class_id = np.argmax(scores)
-                            confidence = scores[class_id]
-                            if confidence > 0.5 and classes[class_id] == "bottle":
-                                print("see a bottle")
-                                w = int(detection[2] * 416)
-                                h = int(detection[3] * 416)
-                                sqare = w * h
-                                result_action = ucb_where_to_fly(sqare)
-                                if dict_of_commands[result_action] == "w":
-                                    ch_3 = min_v
-                                if dict_of_commands[result_action] == "s":
-                                    ch_3 = max_v
-                                if dict_of_commands[result_action] == "a":
-                                    ch_4 = min_v
-                                if dict_of_commands[result_action] == "d":
-                                    ch_4 = max_v
-                                if dict_of_commands[result_action] == "q":
-                                    ch_2 = 2000
-                                if dict_of_commands[result_action] == "e":
-                                    ch_2 = 1000
-                                if dict_of_commands[result_action] == "i":
-                                    ch_1 = 2000
-                                if dict_of_commands[result_action] == "k":
-                                    ch_1 = 1000
-                            else:
-                                """ch_1=1590
-                                ch_2=1600
-                                sqare=0"""
-                                pass
+            confidence, class_id, camera_frame = detection_of_bottle()
+
+            if confidence > 0.5 and classes[class_id] == "bottle":
+                time.sleep(6)
+
+                #     print("see a bottle")
+                #     w = int(detection[2] * 416)
+                #     h = int(detection[3] * 416)
+                #     sqare = w * h
+                #     result_action = ucb_where_to_fly(sqare)
+                #     if dict_of_commands[result_action] == "w":
+                #         ch_3 = min_v
+                #     if dict_of_commands[result_action] == "s":
+                #         ch_3 = max_v
+                #     if dict_of_commands[result_action] == "a":
+                #         ch_4 = min_v
+                #     if dict_of_commands[result_action] == "d":
+                #         ch_4 = max_v
+                #     if dict_of_commands[result_action] == "q":
+                #         ch_2 = 2000
+                #     if dict_of_commands[result_action] == "e":
+                #         ch_2 = 1000
+                #     if dict_of_commands[result_action] == "i":
+                #         ch_1 = 2000
+                #     if dict_of_commands[result_action] == "k":
+                #         ch_1 = 1000
+            else:
+                ch_1 = 1590
+                ch_2 = 1600
+                pass
 
                 cv2.imshow("pioneer_camera_stream", camera_frame)
             key = cv2.waitKey(1)
