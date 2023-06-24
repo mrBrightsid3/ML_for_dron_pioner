fdac56871ab92747f033f1df71dbe7dacfc13642
egg227 <sitnikov-vanyusha@list.ru>
2023-06-24 12:42:32 +0300

    add def pendulum & add def fly_to_bottle

--- main.py
+++ main.py
@@ -7,6 +7,50 @@ pioneer_mini = Pioneer()
 camera = Camera()
 min_v = 1300
 max_v = 1700
+FLAG = False
+sum_alerts = 0
+
+
+def pendulum(sum_alerts):
+    print("pendulum")
+    right_pwoer = 1470
+    left_power = 1530
+    while True:
+        ch_1 = 1500
+        ch_2 = 1500
+        ch_3 = 1500
+        ch_4 = 1500
+        ch_5 = 2000
+        TURN = False
+        confidence, class_id, camera_frame = detection_of_bottle()
+        if confidence > 0.5 and classes[class_id] == "bottle":
+            print("see a bottle")
+            sum_alerts += 1
+            TURN = not (TURN)
+            time.sleep(1)
+        elif TURN == True:
+            right_pwoer += 5
+            ch_2 = right_pwoer  # кручение вправо
+            sum_alerts = 0
+        elif TURN == False:
+            left_power -= 5
+            ch_2 = left_power  # кручение влево
+            sum_alerts = 0
+
+        pioneer_mini.send_rc_channels(
+            channel_1=ch_1,
+            channel_2=ch_2,
+            channel_3=ch_3,
+            channel_4=ch_4,
+            channel_5=ch_5,
+        )
+        time.sleep(0.02)
+        if sum_alerts > 50:
+            fly_to_bottle()
+
+
+def fly_to_bottle():
+    print("fly to bottle")
 
 
 def detection_of_bottle():
@@ -28,9 +72,6 @@ def detection_of_bottle():
                     class_id = np.argmax(scores)
                     confidence = scores[class_id]
                     return confidence, class_id, camera_frame
-
-
-FLAG = False
 
 
 if __name__ == "__main__":
@@ -61,10 +102,10 @@ if __name__ == "__main__":
             ch_4 = 1500
             ch_5 = 2000
             confidence, class_id, camera_frame = detection_of_bottle()
-
             if confidence > 0.5 and classes[class_id] == "bottle":
+                print("see a bottle")
                 time.sleep(6)
-                ch_2 = 1430  # кручение в вправо
+                pendulum()
 
             else:
                 ch_1 = 1590  # поднятие + кручение влево
@@ -110,6 +151,10 @@ if __name__ == "__main__":
                 ch_1 = 1000
             elif key == ord(" "):
                 FLAG = not (FLAG)
+                if FLAG:
+                    print("autopilot on")
+                else:
+                    print("autopilot off")
             elif key == ord("b"):
                 print(pioneer_mini.get_battery_status())
 
@@ -121,7 +166,6 @@ if __name__ == "__main__":
                 channel_5=ch_5,
             )
             time.sleep(0.02)
-            # print(FLAG)
     finally:
         time.sleep(1)
         pioneer_mini.land()
