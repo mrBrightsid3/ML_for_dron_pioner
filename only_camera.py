fedbbdbace530db03f3fd1a033f61854b9af5511
WPE <andronch2002@gmail.com>
2023-07-14 21:53:41 +0300

    print coordinates

--- only_camera.py
+++ only_camera.py
@@ -1,6 +1,11 @@
 import cv2
 import numpy as np
 from pioneer_sdk import Camera
+from get_coordinates import get_coordinates
+
+aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_50)
+aruco_params = cv2.aruco.DetectorParameters()
+aruco_detector = cv2.aruco.ArucoDetector(aruco_dict, aruco_params)
 
 # Connect to the drone camera
 camera = Camera()
@@ -8,6 +13,8 @@ camera = Camera()
 if __name__ == "__main__":
     while True:
         frame = camera.get_cv_frame()  # Get raw data
+        x, y = get_coordinates(frame)
+        print(f"x = {x}, y = {y}")
         cv2.imshow("video", frame)  # Show an image on the screen
 
         if cv2.waitKey(1) == 27:  # Exit if the ESC key is pressed
