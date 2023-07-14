d6f4e89a51dee2f262a97bb28d591b2b4e3e65b3
WPE <andronch2002@gmail.com>
2023-07-14 22:12:22 +0300

    fixed fixed fixed coordinates

--- only_camera.py
+++ only_camera.py
@@ -14,8 +14,11 @@ if __name__ == "__main__":
     while True:
         frame = camera.get_cv_frame()  # Get raw data
         x, y = get_coordinates(frame)
-        print(f"x = {x}, y = {y}")
-        cv2.imshow("video", frame)  # Show an image on the screen
+        try:
+            print(f"x = {x}, y = {y}")
+            cv2.imshow("video", frame)  # Show an image on the screen
+        except:
+            pass
 
         if cv2.waitKey(1) == 27:  # Exit if the ESC key is pressed
             break
