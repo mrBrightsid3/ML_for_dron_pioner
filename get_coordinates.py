10c038f03a410ab54de9ffb92d0232a9182898dc
WPE <andronch2002@gmail.com>
2023-07-14 22:03:51 +0300

    fixed video

--- get_coordinates.py
+++ get_coordinates.py
@@ -1,32 +1,26 @@
 import cv2
-from pioneer_sdk import Pioneer, Camera
 import numpy as np
-
+from pioneer_sdk import Camera
+from get_coordinates import get_coordinates
 
 aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_50)
 aruco_params = cv2.aruco.DetectorParameters()
 aruco_detector = cv2.aruco.ArucoDetector(aruco_dict, aruco_params)
 
+# Connect to the drone camera
+camera = Camera()
 
-def get_coordinates(frame):
-    corners, ids, rejected = aruco_detector.detectMarkers(frame)
-    if np.all(ids is not None):
-        x_center = int(
-            (
-                corners[0][0][0][0]
-                + corners[0][0][1][0]
-                + corners[0][0][2][0]
-                + corners[0][0][3][0]
-            )
-            // 4
-        )
-        y_center = int(
-            (
-                corners[0][0][0][1]
-                + corners[0][0][1][1]
-                + corners[0][0][2][1]
-                + corners[0][0][3][1]
-            )
-            // 4
-        )
-        return x_center, y_center
+if __name__ == "main":
+    frame = camera.get_cv_frame()
+    cv2.imshow("video", frame)
+    while True:
+        try:
+            x, y = get_coordinates(frame)
+            print(f"x = {x}, y = {y}")
+
+            if cv2.waitKey(1) == 27:  # Exit if the ESC key is pressed
+                break
+        except:
+            pass
+
+    cv2.destroyAllWindows()  # Close all opened openCV windows
